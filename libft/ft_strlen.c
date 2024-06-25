3f3d6c168053f94f8c9912ac3e7d54e97162e31c
pcpO4 <137820213+pcpO4@users.noreply.github.com>
2024-06-25 22:55:39 +0200

    Add files via upload

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,15 +6,15 @@
 /*   By: pcervant <pcervant@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 19:50:30 by pcervant          #+#    #+#             */
-/*   Updated: 2024/06/25 22:45:50 by pcervant         ###   ########.fr       */
+/*   Updated: 2024/06/25 22:54:41 by pcervant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
