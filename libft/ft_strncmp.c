c292035caf4f09e03e8d9d3eb128a90982705fa7
pcpO4 <137820213+pcpO4@users.noreply.github.com>
2024-06-25 18:17:26 +0200

    Add files via upload

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pcervant <pcervant@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 13:54:25 by pcervant          #+#    #+#             */
-/*   Updated: 2024/06/25 18:05:26 by pcervant         ###   ########.fr       */
+/*   Updated: 2024/06/25 18:16:54 by pcervant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n && s1[i] != '\0' && s1[i] == s2[i])
+	while (s1[i] != '\0' && s1[i] == s2[i] && i < (n - 1))
 		++i;
 	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
@@ -28,8 +28,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 int main()
 {
-	char *s1 = "test";
-	char *s2 = "tests";
-	printf("Original: %d\n", strncmp(s1, s2, 7));
-	printf("Mio: %d", ft_strncmp(s1, s2, 7));
+	char *s1 = "abcdefgh";
+	char *s2 = "abcdwxyz";
+	printf("Original: %d\n", strncmp(s1, s2, 4));
+	printf("Mio: %d", ft_strncmp(s1, s2, 4));
 }*/
