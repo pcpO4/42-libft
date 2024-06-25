4d03f9c12009b69ea188695b965adc3137fc8096
pcpO4 <137820213+pcpO4@users.noreply.github.com>
2024-06-25 23:24:40 +0200

    Add files via upload

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pcervant <pcervant@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 12:25:39 by pcervant          #+#    #+#             */
-/*   Updated: 2024/06/25 23:20:54 by pcervant         ###   ########.fr       */
+/*   Updated: 2024/06/25 23:24:25 by pcervant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
 	size_t	i;
+	size_t	tam1;
+	size_t	tam2;
 
-	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	tam1 = ft_strlen(s1);
+	tam2 = ft_strlen(s2);
+	str = malloc((tam1 + tam2 + 1) * sizeof(char));
 	i = 0;
-	while (i < ft_strlen(s1))
+	while (i < tam1)
 	{
 		str[i] = s1[i];
 		++i;
 	}
-	while (i < (ft_strlen(s1) + ft_strlen(s2)))
+	while (i < (tam1 + tam2))
 	{
-		str[i] = s2[i - ft_strlen(s1)];
+		str[i] = s2[i - tam1];
 		++i;
 	}
 	str[i] = '\0';
