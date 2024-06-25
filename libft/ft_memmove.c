346a5bf86103340bb4018f5b28e35102506e2ee0
pcpO4 <137820213+pcpO4@users.noreply.github.com>
2024-06-25 22:04:11 +0200

    Add files via upload

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,37 +6,35 @@
 /*   By: pcervant <pcervant@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 12:33:56 by pcervant          #+#    #+#             */
-/*   Updated: 2024/06/25 21:55:13 by pcervant         ###   ########.fr       */
+/*   Updated: 2024/06/25 22:01:12 by pcervant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*dst1;
+	unsigned char	*src1;
 
 	i = 0;
-	if (dest == 0 && src == 0)
-		return (dest);
-	if ((size_t)dest - (size_t)src < n)
+	dst1 = (unsigned char *)dst;
+	src1 = (unsigned char *)src;
+	if (dst == 0 && src == 0)
+		return (dst);
+	while (i < n && &src[0] >= &dst[0])
 	{
-		i = n - 1;
-		while (i < n)
-		{
-			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			--i;
-		}
+		dst1[i] = src1[i];
+		i++;
 	}
-	else
+	i = n;
+	while (i > 0 && &src[0] < &dst[0])
 	{
-		while (i < n)
-		{
-			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			++i;
-		}
+		dst1[i - 1] = src1[i - 1];
+		i--;
 	}
-	return (dest);
+	return (dst);
 }
 /*#include <stdio.h>
 int main  ()
