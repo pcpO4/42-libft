6918c3617ba0c4515adcac4227db6587e1d89144
pcpO4 <137820213+pcpO4@users.noreply.github.com>
2024-06-25 22:50:01 +0200

    Add files via upload

--- libft/ft_lstlast_bonus.c
+++ libft/ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pcervant <pcervant@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 12:38:14 by pcervant          #+#    #+#             */
-/*   Updated: 2024/06/25 22:48:26 by pcervant         ###   ########.fr       */
+/*   Updated: 2024/06/25 22:49:44 by pcervant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 t_list	*ft_lstlast(t_list *lst)
 {
 	while (lst)
+	{
 		if (!lst->next)
 			return (lst);
 		lst = lst->next;
+	}
 	return (lst);
 }
