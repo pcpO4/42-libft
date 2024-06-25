b37639fb0cd6b2836d2bd7261430366e72fe28e8
pcpO4 <137820213+pcpO4@users.noreply.github.com>
2024-06-25 18:04:07 +0200

    Add files via upload

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pcervant <pcervant@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 13:54:25 by pcervant          #+#    #+#             */
-/*   Updated: 2024/06/25 10:58:20 by pcervant         ###   ########.fr       */
+/*   Updated: 2024/06/25 18:02:01 by pcervant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,17 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n && s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	while (i < n && s1[i] != '\0' && s1[i] == s2[i])
 		++i;
-	}
-	return (0);
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
-/*#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
 int main()
 {
-	char *s1 = "atoms\0\0\0\0";
-	char *s2 = "atoms\0abc";
-	printf("Original: %d\n", strncmp(s1, s2, 8));
-	printf("Mio: %d", ft_strncmp(s1, s2, 8));
-}*/
+	char *s1 = "test";
+	char *s2 = "tests";
+	printf("Original: %d\n", strncmp(s1, s2, 7));
+	printf("Mio: %d", ft_strncmp(s1, s2, 7));
+}
